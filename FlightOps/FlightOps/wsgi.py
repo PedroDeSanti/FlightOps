d0fe0d454ac8cec7faa696f7ec2d3f84551eecc4
GabZamba <gabrielzambelli2002@gmail.com>
2022-09-30 16:41:35 -0300

    📝 Added project documentation
    Co-authored-by: jvtdegelo <jvtdegelo@users.noreply.github.com>
    Co-authored-by: Pedro de Santi <PedroDeSanti@users.noreply.github.com>

--- FlightOps/FlightOps/wsgi.py
+++ FlightOps/FlightOps/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyProj project.
+WSGI config for FlightOps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyProj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FlightOps.settings')
 
 application = get_wsgi_application()
