494f03aa88ec0088864dd0c58aa267fbba199fe1
GabZamba <gabrielzambelli2002@gmail.com>
2022-11-11 16:05:43 -0300

    ✨ Add max login attempt
    Co-authored-by: jvtdegelo <jvtdegelo@users.noreply.github.com>
    Co-authored-by: Pedro de Santi <PedroDeSanti@users.noreply.github.com>

--- FlightOps/FlightOps/settings.py
+++ FlightOps/FlightOps/settings.py
@@ -38,7 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
+    'axes',
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'axes.middleware.AxesMiddleware',
 ]
 
 ROOT_URLCONF = 'FlightOps.urls'
