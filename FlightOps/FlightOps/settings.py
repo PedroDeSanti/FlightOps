21e3ae8977d2ba1830deb74f6e42627b74389c1f
Joao Victor Degelo <degelo@usp.br>
2022-12-08 11:12:27 -0300

    🐛 Corrige bug no settings.py para deploy

--- FlightOps/FlightOps/settings.py
+++ FlightOps/FlightOps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qf#+92eg^o5pd+o-mn+-bjowpp4o#+4sj@nk*ht^c-y-_r%u^a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ["degelo.pythonanywhere.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
